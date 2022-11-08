c94561063e58ae85b2149859b834a88e577f5477
dablenparty <hdavenport8902@gmail.com>
2022-11-08 18:19:01 -0500

    Resizes images to add better resolution support

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import ctypes
 import os
+from pathlib import Path
 import traceback
 import psutil
 import signal
 import subprocess as sp
 import time
 
+import cv2
 from python_imagesearch.imagesearch import imagesearch_region_loop
 
 NETSH_COMMAND = 'netsh interface set interface "Ethernet"'
@@ -69,16 +71,31 @@ def kill_process():
     os.kill(int(pid), signal.SIGTERM)
 
 
+def resize_image(image_path: Path, monitor_width: int):
+    img = cv2.imread(str(image_path), cv2.IMREAD_UNCHANGED)
+    scale_percent = monitor_width / 2560
+    width = int(img.shape[1] * scale_percent)
+    height = int(img.shape[0] * scale_percent)
+    new_dim = (width, height)
+    resized_img = cv2.resize(img, new_dim, interpolation=cv2.INTER_AREA)
+    cv2.imwrite(str(image_path.parent / "heist_passed_resized.jpg"), resized_img)
+
+
 def main():
     mon_width, mon_height = get_main_monitor_resolution()
     x1 = 0
     y1 = round(mon_height / 5.76)
     x2 = mon_width
     y2 = y1 + 1000
+    this_file = Path(__file__).resolve()
+    image_path = this_file.parent / "heist_passed_resized.jpg"
+    if not image_path.exists():
+        print("resizing image")
+        resize_image(this_file.parent / "heist_passed_cropped.jpg", mon_width)
+
     print("Searching for image...")
-    image_path = os.path.join(os.path.dirname(__file__), "heist_passed_cropped.jpg")
     # ? TODO: custom impl to cap directly from window
-    pos = imagesearch_region_loop(image_path, 0.2, x1, y1, x2, y2, 0.7)
+    pos = imagesearch_region_loop(str(image_path), 0.2, x1, y1, x2, y2, 0.7)
     if pos[0] == -1:
         print("the image was somehow not found")
         return
@@ -86,7 +103,7 @@ def main():
     time.sleep(1)
     # disable_network()
     kill_process()
-
+    input("press enter to exit...")
 
 if __name__ == "__main__":
     main()
